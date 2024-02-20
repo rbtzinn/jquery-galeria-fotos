a6c7d0eb05e19727687f962efcbbe1913f5bee8a
Roberto Gabriel <roberto.gabriel2004@hotmail.com>
2024-02-20 19:54:08 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function (){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const endereçoDaNovaImagem = $('#endereço-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>')
+        $(`<img src="${endereçoDaNovaImagem}"/>`).appendTo(novoItem)
+        $(` <div class="overlay-imagem-link">
+        <a href="${endereçoDaNovaImagem}" title="Ver imagem no tamanho real" target="_blank"> Ver imagem em tamanho real</a>
+            </div>`).appendTo(novoItem);
+            $(novoItem).appendTo('ul');
+            $(novoItem).fadeIn(1000);
+            $('#endereço-imagem-nova').val('')
+    })
+})
